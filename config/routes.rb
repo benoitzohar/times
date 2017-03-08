89118671d0eebd8c334f1f5498f2783b22a17644
Benoit Zohar <benoit.zohar@gmail.com>
2017-03-08 00:56:33 -0500

    puma config for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
+
 Rails.application.routes.draw do
+  root "hello_world#index"
   get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
