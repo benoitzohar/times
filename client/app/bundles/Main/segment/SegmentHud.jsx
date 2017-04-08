615aaf501456255f6e8bebdb7f47a838dad5489c
Benoit Zohar <benoit.zohar@gmail.com>
2017-04-08 13:07:02 -0400

    update to React 15.5.0, import PropTypes from other lib, update and fix webpack configuration

--- client/app/bundles/Main/segment/SegmentHud.jsx
+++ client/app/bundles/Main/segment/SegmentHud.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import BEMHelper from 'react-bem-helper';
 import { debounce, now, assign } from 'lodash';
 import { millisecondDurationToHumanReadableString } from '../helpers';
--- client/app/bundles/Main/task/Task.jsx
+++ client/app/bundles/Main/task/Task.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import BEMHelper from 'react-bem-helper';
 
 function Task(props) {
